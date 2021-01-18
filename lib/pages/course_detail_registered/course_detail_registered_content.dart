0b1347b53030f9ad624be4d600617eedcec2b586
gabriel-Oak <gabriel.costa41@fatec.sp.gov.br>
2021-01-17 22:38:37 -0300

    layout

--- lib/pages/course_detail_registered/course_detail_registered_content.dart
+++ lib/pages/course_detail_registered/course_detail_registered_content.dart
@@ -1,10 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:page_transition/page_transition.dart';
 import 'package:traineeit/components/back_button.dart';
 import 'package:traineeit/components/header_course.dart';
-import 'package:traineeit/pages/evaluation/evaluation_page.dart';
-import 'package:traineeit/pages/presence/presence_page.dart';
-import 'package:traineeit/utils/LocalUser.dart';
 import 'widget/lesson.dart';
 
 class CourseDetailRegisteredContent extends StatelessWidget {
@@ -27,11 +23,12 @@ class CourseDetailRegisteredContent extends StatelessWidget {
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       Text(
-                        'Design UI/UX ',
+                        'Power BI',
                         style: TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.w700,
-                            color: Theme.of(context).primaryColor),
+                          fontSize: 18,
+                          fontWeight: FontWeight.w700,
+                          color: Theme.of(context).primaryColor,
+                        ),
                       ),
                       Text.rich(
                         TextSpan(
@@ -51,7 +48,7 @@ class CourseDetailRegisteredContent extends StatelessWidget {
                     children: [
                       Chip(
                           backgroundColor: Color.fromRGBO(0, 122, 123, 0.4),
-                          label: Text('12 aulas',
+                          label: Text('4 aulas',
                               style: TextStyle(
                                   fontSize: 12,
                                   color: Colors.white,
@@ -60,7 +57,7 @@ class CourseDetailRegisteredContent extends StatelessWidget {
                       Chip(
                         backgroundColor: Color.fromRGBO(0, 122, 123, 0.4),
                         label: Text(
-                          '30h',
+                          '20h',
                           style: TextStyle(
                             fontSize: 12,
                             color: Colors.white,
@@ -72,146 +69,56 @@ class CourseDetailRegisteredContent extends StatelessWidget {
                   )
                 ],
               ),
-              LocalUser().user.type == 'Aluno'
-                  ? Padding(
-                      padding: const EdgeInsets.symmetric(vertical: 16.0),
-                      child: Column(
-                        children: [
-                          Text(
-                            'Link para Instrutor',
-                            style: TextStyle(
-                                color: Color(0xff555555),
-                                fontWeight: FontWeight.w900,
-                                letterSpacing: .8),
-                          ),
-                          Container(
-                            decoration: BoxDecoration(
-                                color: Colors.grey.shade200,
-                                borderRadius:
-                                    BorderRadius.all(Radius.circular(10))),
-                            child: TextFormField(
-                              minLines: 2,
-                              maxLines: 4,
-                              decoration: InputDecoration(
-                                contentPadding: const EdgeInsets.all(16),
-                                hintText:
-                                    'Cole aqui o link para acesso do seu instrutor',
-                                border: InputBorder.none,
-                                disabledBorder: InputBorder.none,
-                                enabledBorder: InputBorder.none,
-                                focusedBorder: InputBorder.none,
-                                errorBorder: InputBorder.none,
-                                focusedErrorBorder: InputBorder.none,
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    )
-                  : Container(),
-              LocalUser().user.type == 'Professor'
-                  ? Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        children: [
-                          SizedBox(
-                            width: 140,
-                            child: RaisedButton(
-                              onPressed: () {
-                                Navigator.push(
-                                  context,
-                                  PageTransition(
-                                    type: PageTransitionType.fade,
-                                    child: EvaluationPage(),
-                                  ),
-                                );
-                              },
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(10.0),
-                              ),
-                              color: Color(0xff007A7B),
-                              elevation: 0,
-                              child: Text(
-                                'Pauta',
-                                style: TextStyle(
-                                  fontSize: 18,
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.w500,
-                                ),
-                              ),
-                            ),
-                          ),
-                          SizedBox(
-                            width: 140,
-                            child: RaisedButton(
-                              onPressed: () {
-                                Navigator.push(
-                                  context,
-                                  PageTransition(
-                                    type: PageTransitionType.fade,
-                                    child: EvaluationPage(),
-                                  ),
-                                );
-                              },
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(10.0),
-                              ),
-                              color: Color(0xff007A7B),
-                              elevation: 0,
-                              child: Text(
-                                'Pauta',
-                                style: TextStyle(
-                                  fontSize: 18,
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.w500,
-                                ),
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    )
-                  : Container(),
-              LocalUser().user.type == 'Empresa'
-                  ? Padding(
-                      padding: const EdgeInsets.symmetric(
-                          vertical: 16, horizontal: 8),
-                      child: SizedBox(
-                        width: double.infinity,
-                        height: 48,
-                        child: RaisedButton(
-                          onPressed: () {
-                            Navigator.push(
-                              context,
-                              PageTransition(
-                                type: PageTransitionType.fade,
-                                child: PresencePage(id: 1),
-                              ),
-                            );
-                          },
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10.0),
-                          ),
-                          color: Color(0xff007A7B),
-                          elevation: 0,
-                          child: Text(
-                            'Frequencia',
-                            style: TextStyle(
-                              fontSize: 18,
-                              color: Colors.white,
-                              fontWeight: FontWeight.w500,
-                            ),
-                          ),
-                        ),
-                      ),
-                    )
-                  : Container(),
-              Lesson(),
-              Lesson(),
-              Lesson(),
-              Lesson(),
-              Lesson(),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 16.0),
+                child: Text(
+                  'Link para Instrutor',
+                  style: TextStyle(
+                      color: Color(0xff555555),
+                      fontWeight: FontWeight.w900,
+                      letterSpacing: .8),
+                ),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                    color: Colors.grey.shade200,
+                    borderRadius: BorderRadius.all(Radius.circular(10))),
+                child: TextFormField(
+                  minLines: 2,
+                  maxLines: 4,
+                  decoration: InputDecoration(
+                    contentPadding: const EdgeInsets.all(16),
+                    hintText: 'Cole aqui o link para acesso do seu instrutor',
+                    border: InputBorder.none,
+                    disabledBorder: InputBorder.none,
+                    enabledBorder: InputBorder.none,
+                    focusedBorder: InputBorder.none,
+                    errorBorder: InputBorder.none,
+                    focusedErrorBorder: InputBorder.none,
+                  ),
+                ),
+              ),
+              Lesson(
+                title: 'aula 1 | 17/01/2021 às 15:00',
+                name: '1 - Inserção de dados através de Planilha Excel ou CSV',
+                link: 'meet.google.com/iwh-fafy-sqw',
+              ),
+              Lesson(
+                title: 'aula 2 | 18/01/2021 às 15:00',
+                name:
+                    '2 - Conhecimento de Gráficos, Rótulos de Sistema, Hiperlink entre páginas',
+                link: 'meet.google.com/iwh-fafy-sqw',
+              ),
+              Lesson(
+                title: 'aula 3 | 19/01/2021 às 15:00',
+                name: '3 - Organização de dados ETL;',
+                link: 'meet.google.com/iwh-fafy-sqw',
+              ),
+              Lesson(
+                title: 'aula 3 | 20/01/2021 às 15:00',
+                name: '4 - Tratamento de Layout e de dados com o Power Query',
+                link: 'meet.google.com/iwh-fafy-sqw',
+              ),
             ],
           ),
         ),
